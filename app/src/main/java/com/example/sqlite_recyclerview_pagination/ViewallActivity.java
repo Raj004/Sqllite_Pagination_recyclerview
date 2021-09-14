671ef57b72c769e348837e6d8989da58a1decc09
RajShekhar Tiwari <rajshekhartiwari14@gmail.com>
2021-09-14 16:05:40 +0530

    some init msg

---
+++ app/src/main/java/com/example/sqlite_recyclerview_pagination/ViewallActivity.java
@@ -0,0 +1,60 @@
+package com.example.sqlite_recyclerview_pagination;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ViewallActivity extends AppCompatActivity {
+
+    Button show;
+    DatabaseHelper database;
+    RecyclerView recyclerView;
+    RecycleAdapter recycler;
+    List<DataModel> datamodel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewall);
+        show = (Button) findViewById(R.id.view);
+        datamodel =new ArrayList<DataModel>();
+        recyclerView = (RecyclerView) findViewById(R.id.recycle);
+
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database = new DatabaseHelper(ViewallActivity.this);
+                datamodel=  database.getdata();
+                recycler =new RecycleAdapter(datamodel);
+
+
+                Log.i("HIteshdata",""+datamodel);
+                RecyclerView.LayoutManager reLayoutManager =new LinearLayoutManager(getApplicationContext());
+                recyclerView.setLayoutManager(reLayoutManager);
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
+                recyclerView.setAdapter(recycler);
+
+
+            }
+        });
+
+    }
+
+
+
+
+}
